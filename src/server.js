921078a3e34ef5fb3327f2c1c2328db5a6d88e74
boon2017 <chatchai.pummala@gmail.com>
2023-01-21 21:00:01 +0700

    change server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-import express from "express";
+const express = require("express");
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
