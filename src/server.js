a969e9ae17a4bb84cc6b68e4a1efe64d320c595b
boon2017 <chatchai.pummala@gmail.com>
2023-01-21 13:42:37 +0700

    configure server.js

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+
+let app = express();
+
+//config view engine
+viewEngine(app);
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//init web routes
+initWebRoutes(app);
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`App is running at the port: ${port}`);
+});
